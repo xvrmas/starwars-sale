4acd332b885537be5a769282910c21ec4499f5a0
Xavier Mas <xvrmasgarcia@gmail.com>
2022-11-22 18:50:09 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="has-text-centered is-family-secondary">
+    <NavBar/>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'app',
+  components: { NavBar, Footer }
+
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Footer
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Footer',
+}
+</script>
